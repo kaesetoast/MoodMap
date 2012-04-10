dd85b7623a6a8ebf9948fd2385ed85ed45883905
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-04-10 13:58:06 +0200

    Main Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         	new Mopa\BootstrapBundle\MopaBootstrapBundle(),
+            new MoodMap\MainBundle\MoodMapMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
