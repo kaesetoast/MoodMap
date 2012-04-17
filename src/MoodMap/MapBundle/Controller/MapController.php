186c993f083b0c5b14ea0ed0c9427ff2971723e6
Philipp Nowinski <philipp@nowinski.de>
2012-04-17 11:34:41 +0200

    map controller

---
+++ src/MoodMap/MapBundle/Controller/MapController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MoodMap\MapBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MapController extends Controller {
+
+	public function indexAction() {
+		return $this->render('MoodMapMapBundle:Map:index.html.twig');
+	}
+}
