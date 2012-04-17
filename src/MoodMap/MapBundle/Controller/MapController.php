3ece4e65732cabe9027655891c15ceb268414090
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-04-17 15:02:04 +0200

    search action, map controller

--- src/MoodMap/MapBundle/Controller/MapController.php
+++ src/MoodMap/MapBundle/Controller/MapController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace MoodMap\MapBundle\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MapController extends Controller {
@@ -8,5 +10,13 @@ class MapController extends Controller {
 	public function indexAction() {
 		return $this->render('MoodMapMapBundle:Map:index.html.twig');
 	}
-	
+
+	public function searchAction($color, $keyword) {
+		$response = new Response(json_encode($this));
+
+		$response->headers
+				->set("Content-Type", "application/json", "charset=utf-8");
+
+		return $response;
+	}
 }
--- web/js/map.js
+++ web/js/map.js
@@ -2,17 +2,24 @@ Map = {
 	init : function() {
 		$("#searchFieldWrapper").draggable({
 			grid : [ 60, 60 ],
-			stop: function(event, ui) {
-				Map.getColor();
+			stop : function(event, ui) {
+				Map.search();
 			}
 		});
 	},
-	
-	colors: ["violet", "red", "orange", "yellow", "green", "blue"],
-	
-	getColor: function() {
+
+	colors : [ "violet", "red", "orange", "yellow", "green", "blue" ],
+
+	getColor : function() {
 		var offset = $("#searchFieldWrapper").offset().top + 3;
-		return Map.colors[offset/60];
+		return Map.colors[offset / 60];
+	},
+
+	search : function() {
+		var keyword = $("#searchFieldInput").val();
+		$.post("/map/search/" + Map.getColor() + "/" + keyword, function (result) {
+			console.log(result);
+		});
 	}
 };
 $(document).ready(function() {
