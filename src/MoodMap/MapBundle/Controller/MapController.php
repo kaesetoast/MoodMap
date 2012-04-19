a3f69c1bef9e4c8dd742ed43135cfae6b18cd4e3
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-04-19 14:40:17 +0200

    Das Hallo aus dem Service

--- src/MoodMap/MapBundle/Controller/MapController.php
+++ src/MoodMap/MapBundle/Controller/MapController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace MoodMap\MapBundle\Controller;
+use MoodMap\MapBundle\Services\MapService;
+
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,7 +15,6 @@ class MapController extends Controller {
 
 	public function searchAction($color, $keyword) {
 		$response = new Response(json_encode($this));
-
 		$response->headers
 				->set("Content-Type", "application/json", "charset=utf-8");
 
@@ -21,8 +22,10 @@ class MapController extends Controller {
 	}
 	
 	public function createMapAction () {
-		$response = new Response(json_encode($this));
+		$mapService = $this->get("map_service");
 		
+		$response = new Response(json_encode($this));
+		$response->setContent($mapService->createImage());
 		$response->headers
 		->set("Content-Type", "application/json", "charset=utf-8");
 		
