71d977e9726ad0edbd073090a6aecbaa13fc7e01
kaesetoast <philipp@nowinski.de>
2012-05-08 13:43:48 +0200

    login modal

--- src/MoodMap/MainBundle/Services/ModalService.php
+++ src/MoodMap/MainBundle/Services/ModalService.php
@@ -5,8 +5,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ModalService {
 	const PATH = "MoodMapMainBundle:Util:";
-	const LOGIN_MODAL_CONTENT = "login_modal_content.html.twig";
-	
+    const PREFIX = "_modal_content.html.twig";
+
 	private $container;
 	
 	public function __construct(ContainerInterface $container) {
@@ -16,6 +16,6 @@ class ModalService {
 	public function getModalContent($key) {
 		$twigService = $this->container->get("templating");
 
-        return $twigService->renderResponse($this::PATH.$key);
+        return $twigService->renderResponse($this::PATH.$key.$this::PREFIX);
 	}
 }
