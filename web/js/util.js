8d7c84d9c9bc36b9abf18d9ed48215841c27cb24
kaesetoast <philipp@nowinski.de>
2012-05-08 14:14:28 +0200

    login via modal

--- web/js/util.js
+++ web/js/util.js
@@ -1,7 +1,12 @@
 var Util = {
 	Modal: {
-		invoke:function(headline, messageKey, submitButtonText) {
-            $.post("/getmodalcontent/" + messageKey, function(message) {
+
+        ContentLookup: {
+            LOGIN: "/login"
+        },
+
+		invoke:function(headline, modalContent, submitButtonText) {
+            $.post(modalContent, function(message) {
                 $("#modal-headline", "#modal-wrapper").text(headline);
 
                 $($(".modal-body", "#modal-wrapper")[0]).html(message);
