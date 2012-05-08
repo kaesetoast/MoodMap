b70f634ad6c8406bf0437c3664e572782236353b
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-05-08 12:33:35 +0200

    Modal-Logik fertig

--- web/js/util.js
+++ web/js/util.js
@@ -1,11 +1,14 @@
 var Util = {
 	Modal: {
-		invoke:function(headline, message, submitButtonText) {
-			$("#modal-headline", "#modal-wrapper").text(headline);
-			$($(".modal-body", "#modal-wrapper")[0]).html(message);
-			$("#modal-submit", "#modal-wrapper").text(submitButtonText);
-			
-			$("#modal-wrapper").modal("show");
+		invoke:function(headline, messageKey, submitButtonText) {
+            $.post("/getmodalcontent/" + messageKey, function(message) {
+                $("#modal-headline", "#modal-wrapper").text(headline);
+
+                $($(".modal-body", "#modal-wrapper")[0]).html(message);
+                $("#modal-submit", "#modal-wrapper").text(submitButtonText);
+
+                $("#modal-wrapper").modal("show");
+            });
 		}
 	}	
 };
