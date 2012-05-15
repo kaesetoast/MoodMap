567240602e7a6b175929f75505688daf17f26d8a
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-05-15 12:37:34 +0200

    AlertService

---
+++ src/MoodMap/MainBundle/Services/AlertService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nielsgarve
+ * Date: 15.05.12
+ * Time: 12:36
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace MoodMap\MainBundle\Services;
+
+class AlertService {
+    
+}
