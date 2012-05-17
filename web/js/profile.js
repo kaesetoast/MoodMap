f512624d0a39dd33de3d85162ef6e5acd7b0beaf
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-05-17 17:59:24 +0200

    Profile, warum gehen die slider von hier nicht: http://jqueryui.com/demos/slider/#colorpicker? Ich dachte die ColorPicker lösung wäre auch gut für mobile?

---
+++ web/js/profile.js
@@ -0,0 +1,44 @@
+var Profile = {
+
+    toHex:function (r, g, b) {
+        var hex = [r.toString(16), g.toString(16), b.toString(16)];
+        $.each(hex, function (index, value) {
+            if (value.length === 1) {
+                hex[index] = "0" + value;
+            }
+        });
+        return hex.join("").toUpperCase();
+    },
+
+    updatePreview:function () {
+        var red = $("#red").slider("value"),
+            green = $("#green").slider("value"),
+            blue = $("#blue").slider("value"),
+            hex = Profile.toHex(red, green, blue);
+        $("#preview").css("background-color", "#" + hex);
+    },
+
+    init:function () {
+        $("#red, #green, #blue").slider({
+            orientation:"horizontal",
+            range:"min",
+            max:255,
+            value:127,
+            slide:Profile.updatePreview(),
+            change:Profile.updatePreview()
+        });
+        $("#red").slider("value", 255);
+        $("#green").slider("value", 140);
+        $("#blue").slider("value", 60);
+    }
+}
+
+$(document).ready(function () {
+    Profile.init();
+});
+
+
+
+
+
+
