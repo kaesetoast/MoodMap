b432b056cc17c6a9c28df4bea08af91ea85e783e
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-05-18 20:07:31 +0200

    Profile

--- web/js/profile.js
+++ web/js/profile.js
@@ -1,36 +1,50 @@
 var Profile = {
 
-    init:function () {
-        $("#preview").css("background-color", "rgb(127, 127, 127)");
-        $("#red, #green, #blue").slider({
-            orientation:"horizontal",
-            range:"min",
-            max:255,
-            value:127,
-            slide:Profile.updatePreview,
-            change:Profile.updatePreview
-        });
+    Selectable:{
+        init:function () {
+            $(".selectable", "#map-colors").selectable({
+                filter:"div"
+            });
+        }
     },
 
-    updatePreview:function (event, ui) {
-        var red = $("#red").slider("value"),
-            green = $("#green").slider("value"),
-            blue = $("#blue").slider("value"),
-            hex = Profile.toHex(red, green, blue);
-        $("#preview").css("background-color", "#" + hex);
-    },
+    ColorPicker:{
+        init:function () {
+            /*
+             * TODO: Diese Werte müssen aus der DB kommen
+             */
+            $(".ui-selectee").css("background-color", "rgb(127, 127, 127)");
+            $("#red, #green, #blue").slider({
+                orientation:"horizontal",
+                range:"min",
+                max:255,
+                value:127,
+                slide:Profile.ColorPicker.updatePreview,
+                change:Profile.ColorPicker.updatePreview
+            });
+        },
 
-    toHex:function (r, g, b) {
-        var hex = [r.toString(16), g.toString(16), b.toString(16)];
-        $.each(hex, function (index, value) {
-            if (value.length === 1) {
-                hex[index] = "0" + value;
-            }
-        });
-        return hex.join("").toUpperCase();
+        updatePreview:function (event, ui) {
+            var red = $("#red").slider("value"),
+                green = $("#green").slider("value"),
+                blue = $("#blue").slider("value"),
+                hex = Profile.ColorPicker.toHex(red, green, blue);
+            $(".ui-selected").css("background-color", "#" + hex);
+        },
+
+        toHex:function (r, g, b) {
+            var hex = [r.toString(16), g.toString(16), b.toString(16)];
+            $.each(hex, function (index, value) {
+                if (value.length === 1) {
+                    hex[index] = "0" + value;
+                }
+            });
+            return hex.join("").toUpperCase();
+        }
     }
 }
 
 $(document).ready(function () {
-    Profile.init();
+    Profile.Selectable.init();
+    Profile.ColorPicker.init();
 });
