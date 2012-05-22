997166a40261e873a5bdc8080db5f1a5ade34c62
kaesetoast <philipp@nowinski.de>
2012-05-22 11:00:41 +0200

    getmapcolors

---
+++ src/MoodMap/UserBundle/Controller/ProfileController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace MoodMap\UserBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller managing the user profile
+ *
+ */
+class ProfileController extends ContainerAware
+{
+    /**
+     * Show the user
+     */
+    public function showAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'), array('user' => $user));
+    }
+
+    /**
+     * Edit the user
+     */
+    public function editAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $form = $this->container->get('fos_user.profile.form');
+        $formHandler = $this->container->get('fos_user.profile.form.handler');
+
+        $process = $formHandler->process($user);
+        if ($process) {
+            $this->setFlash('fos_user_success', 'profile.flash.updated');
+
+            return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
+        }
+
+        return $this->container->get('templating')->renderResponse(
+            'FOSUserBundle:Profile:edit.html.'.$this->container->getParameter('fos_user.template.engine'),
+            array('form' => $form->createView(), 'theme' => $this->container->getParameter('fos_user.template.theme'))
+        );
+    }
+
+    protected function setFlash($action, $value)
+    {
+        $this->container->get('session')->setFlash($action, $value);
+    }
+
+    public function updateMapColorsAction() {
+        $em = $this->container->get('doctrine')->getEntityManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $user->setMapColors(array("red", "blue", "green", "violet", "black", "white"));
+        $em->persist($user);
+        $em->flush();
+
+        $response = new Response(json_encode(array('success' => true)));
+        $response->headers
+            ->set("Content-Type", "application/json", "charset=utf-8");
+        return $response;
+    }
+
+    public function getMapColorsAction() {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $response = new Response(json_encode($user->getMapColors()));
+        $response->headers
+            ->set("Content-Type", "application/json", "charset=utf-8");
+        return $response;
+    }
+
+}
