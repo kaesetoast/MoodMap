87c1dcd87c98c6522702ea6995e7e91c7d4f4438
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-06-21 21:31:20 +0200

    Emotigramm-Service

--- src/MoodMap/MapBundle/Services/EmotigrammService.php
+++ src/MoodMap/MapBundle/Services/EmotigrammService.php
@@ -11,6 +11,20 @@ namespace MoodMap\MapBundle\Services;
 
 class EmotigrammService {
     public function createEmotigramm() {
+        $emotigramm = array();
+
+        $testString = "Mit regulären Ausdrücken werden explizite Eingaben von Benutzern in Formularen auf ihre Korrektheit überprüft. Damit wird überprüft, ob die eingegebene Zeichenkette dem definierten Muster entspricht. Es wird nun ersichtlich, dass einzugebenden Felder innerhalb eines Formulares mit diesen Mustern versehen werden können. Somit wird nicht nur gewährleistet, dass die gewollten Informationen erhalten werden, es entspricht auch dem Sicherheitsaspekt, sich vor diversen Attacken zu schützen.";
+
+        $sentences = preg_split("/\./", $testString);
+        foreach($sentences as $sentence) {
+
+            $words = preg_split("/\s/", $sentence);
+            foreach($words as $word) {
+
+                echo $word . "\n";
+            }
+        }
+
         return true;
     }
 }
