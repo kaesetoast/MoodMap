9294ce0b24b8adb3c52b45135afc811c530a09a1
alnico87 <nielsgarve@Niels-Garves-MacBook-Pro.local>
2012-07-03 14:22:37 +0200

    hallo

--- src/MoodMap/UserBundle/Controller/ProfileController.php
+++ src/MoodMap/UserBundle/Controller/ProfileController.php
@@ -16,50 +16,6 @@ use FOS\UserBundle\Controller\ProfileController as BaseController;
  */
 class ProfileController extends BaseController
 {
-    /**
-     * Show the user
-     */
-    public function showAction()
-    {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:show.html.' . $this->container->getParameter('fos_user.template.engine'), array('user' => $user));
-    }
-
-    /**
-     * Edit the user
-     */
-    public function editAction()
-    {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-
-        $form = $this->container->get('fos_user.profile.form');
-        $formHandler = $this->container->get('fos_user.profile.form.handler');
-
-        $process = $formHandler->process($user);
-        if ($process) {
-            $this->setFlash('fos_user_success', 'profile.flash.updated');
-
-            return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
-        }
-
-        return $this->container->get('templating')->renderResponse(
-            'FOSUserBundle:Profile:edit.html.' . $this->container->getParameter('fos_user.template.engine'),
-            array('form' => $form->createView(), 'theme' => $this->container->getParameter('fos_user.template.theme'))
-        );
-    }
-
-    protected function setFlash($action, $value)
-    {
-        $this->container->get('session')->setFlash($action, $value);
-    }
-
     public function updateMapColorsAction()
     {
         $request = $this->container->get('request')->request;
@@ -70,9 +26,11 @@ class ProfileController extends BaseController
         $em->flush();
 
         $mapService = $this->container->get("map_service");
-        $mapService->createImage();
 
-        return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
+        $response = new Response(json_encode(array('success' => $mapService->createImage())));
+        $response->headers
+            ->set("Content-Type", "application/json", "charset=utf-8");
+        return $response;
     }
 
     public function getMapColorsAction()
